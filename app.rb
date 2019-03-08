dbdf91512dd243f0b5143965dc3d58cf4280f7c0
Brandon Teich <brandonteich123@gmail.com>
2019-03-08 00:41:39 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,10 +7,19 @@ class App < Sinatra::Base
 
   post '/reverse' do
     erb :reversed
+    original_string = params["string"]
+    reversed_string = original_string.reverse
+    erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
-
+   @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+ 
+  erb :friends
   end
+
+<% @friends.each do |friend| %>
+  <h2><%= friend %></h2>
+<% end %>
 end
